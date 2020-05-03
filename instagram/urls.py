5e8dbc43127380c52279ea58b2c9b425e0fbc850
janeshaw1997 <janeshaw.smile@gmail.com>
2020-05-02 21:10:58 -0400

    Succcessfully implemented create, update, delete posts feature. Able to show master view and detail view.

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,8 +15,13 @@ Including another URLconf (can be the URLconf of apps)
 """
 from django.contrib import admin
 from django.urls import include, path
-from instagram.views import HelloWorld
+from instagram.views import HelloWorld, PostsView, PostsDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', HelloWorld.as_view(), name='helloworld'),
+    path('posts/', PostsView.as_view(), name='posts'),
+    path('post/<int:pk>/', PostsDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='make_post'),
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
 ]
